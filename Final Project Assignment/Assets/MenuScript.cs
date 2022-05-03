d7d980c5ebc46c6e78724f20d6dadc42dd9f9d69
Jiachen Tang <tjc9606ok@gmail.com>
2022-05-03 07:58:53 -0400

    menu scene and script completed

---
+++ Final Project Assignment/Assets/MenuScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButton()
+    {
+        Debug.Log("Play Button Pressed");
+        EditorSceneManager.LoadScene(1);
+    }
+
+    public void ControlButton()
+    {
+        Debug.Log("Control Button Pressed");
+        EditorSceneManager.LoadScene(2);
+    }
+
+    public void ExitButton()
+    {
+        Debug.Log("Exit Button Pressed");
+        Application.Quit();
+    }
+
+}
