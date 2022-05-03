22e0f1ce33a23b8b9d89cf96f6d6e332aec6c12d
Jiachen Tang <tjc9606ok@gmail.com>
2022-05-03 08:16:59 -0400

    control scene draft and buttons' script completed

---
+++ Final Project Assignment/Assets/ControlPageButtonsScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class ControlPageButtonsScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGameButton()
+    {
+        Debug.Log("Start Game Button Pressed");
+        EditorSceneManager.LoadScene(1);
+    }
+
+    public void BackToMenuButton()
+    {
+        Debug.Log("Back To Menu Button Pressed");
+        EditorSceneManager.LoadScene(0);
+    }
+}
