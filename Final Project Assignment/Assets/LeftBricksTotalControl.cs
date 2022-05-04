f4d8ed9ed791ae5474dd20ee7456fdc01d9817b5
Jiachen Tang <tjc9606ok@gmail.com>
2022-05-04 04:14:28 -0400

    Left Bricks Total Control script updated, bricks will not move if all of them are disabled now

--- Final Project Assignment/Assets/LeftBricksTotalControl.cs
+++ Final Project Assignment/Assets/LeftBricksTotalControl.cs
@@ -9,6 +9,8 @@ public class LeftBricksTotalControl : MonoBehaviour
     private float speedDirection = 0;
     public float moveSpeed = 1;
 
+    public List<Transform> leftBricks = new List<Transform>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,21 +20,24 @@ public class LeftBricksTotalControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x > -4.9f && speedDirection == 0)
+        if (disabledBricksCount < leftBricks.Count)
         {
-            transform.Translate(new Vector3(moveSpeed * -0.0075f, 0, 0));
-        }
-        else if (transform.position.x <= -4.9f && speedDirection == 0)
-        {
-            speedDirection = 1;
-        }
-        else if (transform.position.x < -3.76f && speedDirection == 1)
-        {
-            transform.Translate(new Vector3(moveSpeed * 0.0075f, 0, 0));
-        }
-        else if (transform.position.x >= -3.76f && speedDirection == 1)
-        {
-            speedDirection = 0;
+            if (transform.position.x > -4.9f && speedDirection == 0)
+            {
+                transform.Translate(new Vector3(moveSpeed * -0.0075f, 0, 0));
+            }
+            else if (transform.position.x <= -4.9f && speedDirection == 0)
+            {
+                speedDirection = 1;
+            }
+            else if (transform.position.x < -3.76f && speedDirection == 1)
+            {
+                transform.Translate(new Vector3(moveSpeed * 0.0075f, 0, 0));
+            }
+            else if (transform.position.x >= -3.76f && speedDirection == 1)
+            {
+                speedDirection = 0;
+            }
         }
     }
 
