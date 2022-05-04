c20121b5bb0e1e03c9d6f019ae3742c2a2cc1e7f
Jiachen Tang <tjc9606ok@gmail.com>
2022-05-04 04:06:38 -0400

    Left Bricks Total Control script updated, the bricks can move automatically now

--- Final Project Assignment/Assets/LeftBricksTotalControl.cs
+++ Final Project Assignment/Assets/LeftBricksTotalControl.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class LeftBricksTotalControl : MonoBehaviour
 {
     private float disabledBricksCount = 0;
+    // 0 means moving towards left, 1 means moving towards right
+    private float speedDirection = 0;
+    public float moveSpeed = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,22 @@ public class LeftBricksTotalControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x > -4.9f && speedDirection == 0)
+        {
+            transform.Translate(new Vector3(moveSpeed * -0.0075f, 0, 0));
+        }
+        else if (transform.position.x <= -4.9f && speedDirection == 0)
+        {
+            speedDirection = 1;
+        }
+        else if (transform.position.x < -3.76f && speedDirection == 1)
+        {
+            transform.Translate(new Vector3(moveSpeed * 0.0075f, 0, 0));
+        }
+        else if (transform.position.x >= -3.76f && speedDirection == 1)
+        {
+            speedDirection = 0;
+        }
     }
 
     public void DisabledCountIncrease()
