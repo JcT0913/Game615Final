4819ed2bd39d8b6b6bb5d641b2e00e88c8af6fcb
Jiachen Tang <tjc9606ok@gmail.com>
2022-05-04 02:56:38 -0400

    Ball Movement, Player Control, Restart Level scripts added

---
+++ Final Project Assignment/Assets/PlayerControl1.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl1 : MonoBehaviour
+{
+    public float speed = 1;
+    public Transform ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.A) && transform.position.x > -7.66f)
+        if (Input.GetKey(KeyCode.A) && transform.position.x > (-8.49f + transform.localScale.x / 2))
+        {
+            transform.Translate(new Vector3(speed * -0.025f, 0, 0));
+        }
+        //else if (Input.GetKey(KeyCode.D) && transform.position.x < -1.04f)
+        else if (Input.GetKey(KeyCode.D) && transform.position.x < (-0.26f - transform.localScale.x / 2))
+        {
+            transform.Translate(new Vector3(speed * 0.025f, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.W) && transform.position.y < -1f)
+        {
+            transform.Translate(new Vector3(0, speed * 0.025f, 0));
+        }
+        else if (Input.GetKey(KeyCode.S) && transform.position.y > -4.43f)
+        {
+            transform.Translate(new Vector3(0, speed * -0.025f, 0));
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            ball.SendMessage("ResetBallPosition");
+        }
+    }
+
+    public void Punishment()
+    {
+        if (transform.localScale.x > 1)
+        {
+            transform.localScale += new Vector3(-0.1f, 0, 0);
+        }
+    }
+}
---
+++ Final Project Assignment/Assets/RestartLevel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartLevel : MonoBehaviour
+{
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            collision.gameObject.transform.position = new Vector3(-4.4f, -0.66f, 0);
+            collision.gameObject.GetComponent<BallMovement>().StartOfBall();
+
+            player.SendMessage("Punishment");
+        }
+    }
+}
